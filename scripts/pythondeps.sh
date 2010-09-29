83251b6b126aa79e1ae5274c637d759aa666e028
Yuri Dario <ydario@users.noreply.github.com>
2010-09-29 20:02:51 +0000

    rpm: better pythondeps implementation.

--- scripts/pythondeps.sh
+++ scripts/pythondeps.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# TODO fix this script to accept $2 instead of stdin
-# re-read file to stdin...
-exec 0<$2
-
 [ $# -ge 1 ] || {
     cat > /dev/null
     exit 0
@@ -12,13 +8,11 @@ exec 0<$2
 PYVER=`python -c "import sys; v=sys.version_info[:2]; print '%d.%d'%v"`
 case $1 in
 -P|--provides)
-    shift
-    grep "/usr/bin/python\*\$" >& /dev/null && echo "python(abi) = ${PYVER}"
+    cat $2 | grep "/usr/bin/python\*\$" > /dev/null && echo "python(abi) = ${PYVER}"
     exit 0
     ;;
 -R|--requires)
-    shift
-    grep "/usr/lib[^/]*/python${PYVER}/" >& /dev/null && echo "python(abi) = ${PYVER}"
+    cat $2 | grep "/usr/lib[^/]*/python${PYVER}/" > /dev/null && echo "python(abi) = ${PYVER}"
     exit 0
     ;;
 esac
