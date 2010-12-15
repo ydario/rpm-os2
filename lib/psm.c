c00867f9af75bf3c09a4f87c7e8eaa3635772a2d
Yuri Dario <ydario@users.noreply.github.com>
2010-12-15 11:48:33 +0000

    rpm: parse a temporary string for script name, fixes ticket:9.

--- lib/psm.c
+++ lib/psm.c
@@ -706,8 +706,10 @@ static rpmRC runScript(rpmpsm psm, Header h, rpmTag stag, ARGV_t * argvp,
 #ifdef __KLIBC__
 	{
 
+		// strtok writes to buffer, this corrupts rpm header data
+		char* _script = strdup( script);
 		// if script is a .cmd or .exe, execute it directly instead of using sh
-		char* token = strtok( script, " ");
+		char* token = strtok( _script, " ");
 		strlwr( token);
 
 		// get native paths
@@ -722,6 +724,7 @@ static rpmRC runScript(rpmpsm psm, Header h, rpmTag stag, ARGV_t * argvp,
 			shell = "sh.exe";
 		}
 		argvAdd(argvp, fn_native);
+		free(_script);
 	}
 
 #else
