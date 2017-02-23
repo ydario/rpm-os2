f9a0e8217601d2ba2f2cb7938211128d2f36cf0d
Dmitriy Kuminov <dmik@users.noreply.github.com>
2017-02-23 21:56:19 +0000

    rpm: Fix scriptlet execution (r1020 regression).
    It would start scriptlets with PATH set to a wrong string
    which completely breaks any script execution (and can also
    make fork fail with ENOENT).

--- lib/rpmscript.c
+++ lib/rpmscript.c
@@ -156,7 +156,7 @@ static rpmRC runLuaScript(rpmPlugins plugins, ARGV_const_t prefixes,
     return rc;
 }
 
-static const char * const SCRIPT_PATH = "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin";
+static const char * const SCRIPT_PATH = "/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin";
 
 static void doScriptExec(ARGV_const_t argv, ARGV_const_t prefixes,
 			FD_t scriptFd, FD_t out)
@@ -196,12 +196,21 @@ static void doScriptExec(ARGV_const_t argv, ARGV_const_t prefixes,
     }
 
     {   char *ipath = rpmExpand("%{_install_script_path}", NULL);
+#ifdef __OS2__
+	/* On OS/2, inherit parent PATH unless overriden with %_install_script_path
+	 * as some tools may be defined w/o path in macros and they need to be
+	 * located dynamically at scriptlet run time.
+	 */
+	if (ipath && *ipath != '%')
+	    xx = setenv("PATH", ipath, 1);
+#else
 	const char *path = SCRIPT_PATH;
 
-	if (ipath && ipath[5] != '%')
+	if (ipath && *ipath != '%')
 	    path = ipath;
 
 	xx = setenv("PATH", path, 1);
+#endif
 	free(ipath);
     }
 
